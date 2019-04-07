e718222fbe7229fb1c49bdf9e9e84c603b65a4c8
dookadoo <35851914+dookadoo@users.noreply.github.com>
2019-04-07 12:56:30 -0600

    Create Reviews.js

---
+++ Reviews.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DB, { useNewUrlParser: true } );
+mongoose.set('useCreateIndex', true);
+//Reviews------------------------------------------
+//{
+//  "title": Title
+//  "by": Username
+//  "quote": quote
+//  "rating": rating
+//}
+var ReviewSchema = new Schema({
+    title: {type: String, required: true, index: { unique: true}},
+    by: {type: String, required: true, index: { unique: true}},
+	quote: {type: String},
+    rating: {type: String, enum: ['1 Star',  '2 Star',  '3 Star',  '4 Star',  '5 Star'], required: true},
+});
+
+
+// return the model
+module.exports = mongoose.model('Movies', MovieSchema);
