839de0efc57de9259a8c7bcdaf5f8f0da7348978
dookadoo <35851914+dookadoo@users.noreply.github.com>
2019-04-07 15:29:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -195,7 +195,7 @@ router.get('/movies/get', function(req, res) {
 			var response;
 			movie.find(function (err, movies) {
 					if (err) res.send(err);
-					res.write(JSON.stringify({movies}));
+					res.write(movies);
 				});
 			/*review.find(function (err2, reviews) {
 					if (err2) res.send(err2);
