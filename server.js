bf74f383f4d92780b87f1549c8722044f0bae589
dookadoo <35851914+dookadoo@users.noreply.github.com>
2019-04-07 16:31:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -249,7 +249,7 @@ router.delete('/movies/delete', function(req, res) {
 });
 //------------------------------------------------
 
-router.post('/reviews/post', function(req, res) {
+router.post('/reviews/add', function(req, res) {
 	var userNew = new User();
     var reviewNew = new review();
     userNew.name = req.headers.name;
